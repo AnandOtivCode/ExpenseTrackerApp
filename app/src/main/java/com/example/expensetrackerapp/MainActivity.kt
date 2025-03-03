02462eacdcd8583260af92f96e099c6e0840bb13
“Anand <w0858857@myscc.ca>
2025-03-03 18:04:53 -0500

    - Setup the XML for expense name, amount and add expense button.

--- app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.expensetrackerapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,16 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Get references to UI elements
+        val expenseNameInput = findViewById<EditText>(R.id.expenseInput)
+        val amountInput = findViewById<EditText>(R.id.amountInput)
+        val submitButton = findViewById<Button>(R.id.addExpenseButton)
+
+
+
+
+
     }
-}
+    }
